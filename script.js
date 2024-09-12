e0770b939a65f446c32262c816f19a630e337565
abdul devoloper <shumaila.anees26@gmail.com>
2024-09-12 22:39:21 +0500

    task done

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Adding product to cart
+document.querySelectorAll('.btn-primary').forEach(button => {
+    button.addEventListener('click', function () {
+        alert('Product added to cart!');
+    });
+});
